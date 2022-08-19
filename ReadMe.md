90bb589e9db60f9fcee9ae9303f74170d935f908
root <nameerrror3301@gmail.com>
2022-08-19 16:13:19 +0200

    Creating ReadMe file

---
+++ ReadMe.md
@@ -0,0 +1,16 @@
+# ParceCoinGeckoExchange
+
+## This script is a commercial product. 
+
+    It simply collects all the exchanges from the site CoinGecko.
+
+    Perhaps you could think of doing everything together, parsing coins and parsing exchanges?
+
+    1 - At the request of the client it was made by a separate program.
+
+    2 - Number of requests per minute on the site 50. Given that you have a free subscription.
+
+    3 - I publish my projects as they are. And how they were commissioned ))
+# Description
+
+    I will be very happy to receive your forks and commits with a clear explanation of where and how to do it better. Thank you for your time.
--- main.go
+++ main.go
@@ -1,5 +1,58 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
 
+	"github.com/xuri/excelize/v2"
+)
+
+type DataExchange struct {
+	Name string `json:"name"`
+}
+
+var (
+	NameList []string
+)
+
+func main() {
+	timeStart := time.Now()
+	f := excelize.NewFile()
+	var data []DataExchange
+	for i := 1; i < 5+1; {
+		url := fmt.Sprintf("https://api.coingecko.com/api/v3/exchanges?per_page=100&page=%d", i)
+		resp, err := http.Get(url)
+		if err != nil {
+			log.Println(err)
+		}
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Println(err)
+		}
+
+		err = json.Unmarshal(body, &data)
+		if err != nil {
+			log.Println("The maximum number of requests per minute was exceeded. Performing blocking bypass...")
+			time.Sleep(70 * time.Second)
+		}
+		fmt.Printf("Visited page - %d of %d\n", i, 5)
+
+		for _, val := range data {
+			NameList = append(NameList, val.Name)
+		}
+
+		for idx, val := range NameList {
+			f.SetCellValue("Sheet1", fmt.Sprintf("A%d", idx+1), val)
+		}
+		i++
+	}
+	if err := f.SaveAs("ExchangeList.xlsx"); err != nil {
+		log.Fatalf("Excel file creation error - %s", err)
+	}
+	fmt.Println("Total execution time - ", time.Since(timeStart))
 }
